ca48153c899a7fbca67fcc60fa64c502f06978df
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-07-02 14:55:34 +0200

    Fix order of dict merges

--- tljh/configurer.py
+++ tljh/configurer.py
@@ -38,7 +38,7 @@ def apply_yaml_config(path, c):
     if os.path.exists(path):
         with open(path) as f:
             # FIXME: Figure out correct order of merging here
-            tljh_config = _merge_dictionaries(yaml.safe_load(f), default)
+            tljh_config = _merge_dictionaries(default, yaml.safe_load(f))
     else:
         tljh_config = copy.deepcopy(default)
 
