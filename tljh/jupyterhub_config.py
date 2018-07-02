024b7e0375745036d65ab5a43a478868c06c627a
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-07-02 15:02:31 +0200

    Remove all this configurableHTTPProxy stuff for now

--- tljh/jupyterhub_config.py
+++ tljh/jupyterhub_config.py
@@ -32,11 +32,11 @@ c.JupyterHub.ssl_cert = '/etc/letsencrypt/live/course.magicc.org/fullchain.pem'
 # redirect http queries to https
 c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
 
-# Use a high port so users can try this on machines with a JupyterHub already present
-c.JupyterHub.hub_port = 15001
+# # Use a high port so users can try this on machines with a JupyterHub already present
+# c.JupyterHub.hub_port = 15001
 
-c.ConfigurableHTTPProxy.should_start = False
-c.ConfigurableHTTPProxy.api_url = 'http://127.0.0.1:15002'
+# c.ConfigurableHTTPProxy.should_start = False
+# c.ConfigurableHTTPProxy.api_url = 'http://127.0.0.1:15002'
 
 c.SystemdSpawner.extra_paths = [os.path.join(USER_ENV_PREFIX, 'bin')]
 c.SystemdSpawner.default_shell = '/bin/bash'
